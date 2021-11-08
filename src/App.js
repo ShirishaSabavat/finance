d7046ac68ece5f5a1a44b368d337f61a9ed212c0
ShirishaSabavat <shirisha660@gmail.com>
2021-11-08 23:01:27 +0530

    displayed backend data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({
+    userName: "shirisha",
+    email: "shirisha660@gmail.com",
+    password: "123456",
+    conformPassword: "123456"
+  })
+  const {userName, email, password, conformPassword} = data;
+  const onChangeEventHandler = e => {
+    setData({...data, [e.target.name] : e.target.value})
+  }
+  const onSubmitEventHandler = e => {
+    e.preventDefault();
+    if (userName.length < 5){
+      alert("userName must be atleast 5 characters")
+    }
+    else if (password!==conformPassword){
+      alert("Passwords are not matching")
+    }else{
+      console.log(data);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <center>
+      <form onSubmit={onSubmitEventHandler}>
+        <input type="text" name="userName" value={userName} onChange={onChangeEventHandler} /> <br />
+        <input type="email" name="email" value={email} onChange={onChangeEventHandler} /> <br />
+        <input type="password" name="password" value={password} onChange={onChangeEventHandler} /> <br />
+        <input type="password" name="conformPassword" value={conformPassword} onChange={onChangeEventHandler} /> <br />
+        {password===conformPassword? null: <p>Passwords are not matching</p>}
+        <input type="submit" name="submit" />
+      </form>
+      <p>{data.userName}</p>
+      <p>{data.email}</p>
+      <p>{data.password}</p>
+      <p>{data.conformPassword}</p>
+      </center>
+      </div>
   );
 }
 
